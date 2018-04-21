fec61c6700d001a68fd0e2fa509d3c5b2e76d3df
Loucil Rubio <mvr12@my.fsu.edu>
2018-04-21 15:50:23 -0400

    Remove Route wrapper from root

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './js/App';
 import './assets/index.css';
-import { Route } from 'react-router-dom';
 
 ReactDOM.render(
-  <Route basename={process.env.PUBLIC_URL}>
-      <App/>
-  </Route>,
-  document.getElementById('root')
+    <App/>,
+    document.getElementById('root')
 );
